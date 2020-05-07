41bf8a45d0b4be2ee71bb3bae6376abd73f0ed46
Progi1984 <franck.lefevre@prestashop.com>
2020-05-07 17:21:26 +0200

    Fixed BackOffice Notification when Settings disable it

--- controllers/admin/AdminAjaxFaviconBOController.php
+++ controllers/admin/AdminAjaxFaviconBOController.php
@@ -14,11 +14,19 @@
 * International Registered Trademark & Property of PrestaShop SA
 */
 
+use PrestaShop\PrestaShop\Core\Domain\Notification\Query\GetNotificationLastElements;
+use PrestaShop\PrestaShop\Core\Domain\Notification\QueryResult\NotificationsResults;
+use PrestaShop\PrestaShop\Adapter\SymfonyContainer;
+
 class AdminAjaxFaviconBOController extends ModuleAdminController
 {
     public function ajaxProcessGetNotifications()
     {
-        $notification = new Notification;
-        $this->ajaxDie(json_encode($notification->getLastElements()));
+        /** @var NotificationsResults $elements */
+        $elements = SymfonyContainer::getInstance()->get('prestashop.core.query_bus')->handle(
+            new GetNotificationLastElements(Context::getContext()->employee->id)
+        );
+        header('Content-Type: application/json');
+        $this->ajaxRender(json_encode($elements->getNotificationsResultsForJS()));
     }
 }
